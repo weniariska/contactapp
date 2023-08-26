6d3fe87d1199db27cbbe8c8ea5cc2bd10dad5993
weniariska <weniariska1@gmail.com>
2023-08-26 11:13:43 +0700

    db local

--- utils/db.js
+++ utils/db.js
@@ -1,18 +1,18 @@
 const mongoose = require('mongoose')
 // via mongodb local
-// mongoose.connect('mongodb://127.0.0.1/contactapp', {
-//     useNewUrlParser: true, 
-//     useUnifiedTopology: true,
-//     useCreateIndex: true,
-// })
+mongoose.connect('mongodb://127.0.0.1/contactapp', {
+    useNewUrlParser: true, 
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+})
 
 // via mongodb atlas
-mongoose.connect("mongodb+srv://weniariska:we6thofmaymongodb@cluster0.d52dpqy.mongodb.net/contactapp?retryWrites=true&w=majority", 
-{
-    useNewUrlParser: true,
-    useFindAndModify: false,
-    useUnifiedTopology: true
-})
+// mongoose.connect("mongodb+srv://weniariska:we6thofmaymongodb@cluster0.d52dpqy.mongodb.net/contactapp?retryWrites=true&w=majority", 
+// {
+//     useNewUrlParser: true,
+//     useFindAndModify: false,
+//     useUnifiedTopology: true
+// })
 
 // // Menambah 1 data mengikuti blueprint
 // const contact1 = new Contact({
